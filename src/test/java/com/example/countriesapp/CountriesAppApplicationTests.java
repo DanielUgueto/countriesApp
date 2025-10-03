362e430c3f9a8a73bdb555fdc170bb8a8c6ce9ac
DanielUgueto <daug0001@stud.kea.dk>
2025-10-03 09:23:35 +0200

    initial commit

---
+++ src/test/java/com/example/countriesapp/CountriesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countriesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
